f1c7731cf90f5436055b8e2f5bb8e680b48e4724
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-09-02 14:50:53 +0200

    Update __init__.py

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,1 +1,9 @@
+# src/parsers/__init__.py
+from . import horizon, erasmus
 
+PARSERS = {
+    "Horizon Europe": horizon.parse_horizon_pdf,
+    "Erasmus+": erasmus.parse_erasmus_pdf,   # add others later
+}
+
+PROGRAMMES = list(PARSERS.keys())
