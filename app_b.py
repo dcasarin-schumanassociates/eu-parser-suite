2ff093bf02c3d0add31cd2da48cf25d77f0c52ca
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-09-04 15:49:58 +0200

    Update app_b.py

--- app_b.py
+++ app_b.py
@@ -377,7 +377,7 @@ def build_altair_chart_from_segments(seg: pd.DataFrame, view_start, view_end):
     chart = (
         month_shade + month_grid + bars + start_labels + end_labels + inbar + month_labels
     ).properties(
-        height=chart_height+60, width=5000
+        height=chart_height, width=5000
     ).configure_axis(
         grid=False
     ).configure_view(
