b02c713a2d00c769a020841fca280fe0fc77031c
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-09-05 12:05:27 +0200

    Update app_b.py

--- app_b.py
+++ app_b.py
@@ -290,7 +290,7 @@ def build_altair_chart_from_segments(seg: pd.DataFrame, view_start, view_end):
             sort=y_order,
             axis=alt.Axis(
                 title=None,
-                labelLimit=500,
+                labelLimit=200,
                 labelFontSize=11,
                 labelAlign="right",
                 labelPadding=50,
