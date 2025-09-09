48e98fc114a9256956d24cb9600c233293ccc9f8
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-09-09 14:13:11 +0200

    Update dashboard_3.0.py

--- dashboard_3.0.py
+++ dashboard_3.0.py
@@ -234,7 +234,7 @@ def gantt_singlebar_chart(g: pd.DataFrame, color_field: str = "type_of_action", 
         x="t:T", tooltip=[alt.Tooltip("t:T", title="Today", format="%d %b %Y")]
     )
     today_label = alt.Chart(today_df).mark_text(
-        align="left", baseline="top", dx=4, dy=12, fontSize=11, fontWeight="bold", color="#4169E1"
+        align="left", baseline="top", dx=4, dy=18, fontSize=11, fontWeight="bold", color="#4169E1"
     ).encode(x="t:T", y=alt.value(0), text=alt.Text("t:T", format='Today: "%d %b %Y"'))
 
     y_order = g["y_label"].drop_duplicates().tolist()
